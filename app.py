35af43489fe9461266c49115b0dfd90f64ccd5f4
akt2 <atwitchellheyne@gmail.com>
2019-06-13 00:04:35 -0400

    flask and stuff

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+import flask import (Flask,render_template,jsonify,request,redirect)
+
+app=Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+db=SQLAlchemy(app)
+
+from .models import Pet
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

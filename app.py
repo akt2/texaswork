075e3be9b6cf2a8f3fc771c33ec25fb3a76f1bd5
akt2 <atwitchellheyne@gmail.com>
2019-06-13 20:58:36 -0400

    more

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ from flask import (
 
 app=Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-db=SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+# db=SQLAlchemy(app)
 
-from .models import Pet
+# from .models import Pet
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return 'meow'
 
 if __name__ == '__main__':
     app.run(debug=True)

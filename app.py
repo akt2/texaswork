5c5b4765d4990273323969bf6aaf49ff165c52d7
akt2 <atwitchellheyne@gmail.com>
2019-06-20 00:54:36 -0400

    added cams nanda code letting ClearDB and flask communicate

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 import os 
 from flask import Flask,render_template,jsonify,request,redirect
+import pymysql
+import sqlalchemy
+import flask_sqlalchemy
+import pandas
+
+# Thank you, Cam! source for the following code up to app=Flask(_name_): https://github.com/CamKirk/nanda/blob/master/app.py
+
+if not os.environ.get('DYNO'):
+    import config
+    print(config.name)
+
+if os.environ.get('CLEARDB_DATABASE_URL'):
+    dburl = os.environ['CLEARDB_DATABASE_URL']
+else:
+    dburl = config.dburl
+
+engine = sqlalchemy.create_engine(dburl)
+df = pandas.read_sql('SELECT * FROM may', engine)
+print(df)
 
 app=Flask(__name__)
-
-from flask_sqlalchemy import SQLAlchemy
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-# db=SQLAlchemy(app)
-
-# from .models import Pet
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/data')
+def data():
+    return jsonify(df.to_json(orient='records'))
 
 if __name__ == '__main__':
     app.run(debug=True)

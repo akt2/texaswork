b8cb5d630db6b35dc3859cd5b625cd81b1eb1748
akt2 <atwitchellheyne@gmail.com>
2019-08-01 00:13:05 -0400

    models page update

--- static/js/vz.js
+++ static/js/vz.js
@@ -32,3 +32,15 @@ function newFunction() {
     return (divElement.offsetWidth * 0.75) + 'px';
 }
 
+window.onscroll = function() {myFunction()};
+    
+    var header = document.getElementById("navy");
+    var sticky = header.offsetTop;
+    
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
